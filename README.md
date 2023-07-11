70ab25deee9dea79badce36986281ca66debb8b3
Tai Le <taiminhle4920@gmail.com>
2023-07-11 16:13:50 -0700

    macos file setup

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # os-setup
+
+## mac-setup ##
+
+### install homebrew ###
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+### change mod of the shell ###
+chmod +x app.sh
+
+### run the script ###
+./app.sh
---
+++ mac-setup/app-install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Update Homebrew
+brew update
+
+# Install apps using Homebrew
+brew install --cask iterm2
+brew install --cask google-chrome
+brew install --cask discord
+brew install --cask mos
+brew install --cask alfred
+brew install --cask aldente
+brew install --cask visual-studio-code
+brew install --cask zoom
+brew install --cask slack
+brew install --cask postman
+
+
+
+echo "App installation completed."
+
