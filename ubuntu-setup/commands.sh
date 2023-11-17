2f21a1cde12643b36789a1524991d5c0b2e74778
Tai Le <66931054+taiminhle4920@users.noreply.github.com>
2023-11-16 22:01:00 -0800

    Delete ubuntu-setup directory

--- ubuntu-setup/commands.sh
+++
@@ -1,9 +0,0 @@
-sudo apt update
-sudo apt upgrade
-sudo apt install gnome-tweaks
-sudo apt install ubuntu-restricted-extras
-sudo apt install snapd
-sudo apt install flatpak
-sudo apt install gnome-software-plugin-flatpak
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-
