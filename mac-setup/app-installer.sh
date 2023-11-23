1618af009b9614765f5cbb5b529d779ac418a1c1
Tai Le <66931054+taiminhle4920@users.noreply.github.com>
2023-11-23 04:45:29 -0800

    Update app-installer.sh

--- mac-setup/app-installer.sh
+++ mac-setup/app-installer.sh
@@ -6,14 +6,14 @@ brew update
 # Install apps using Homebrew
 brew install --cask iterm2
 brew install --cask google-chrome
-brew install --cask brave-browser
 brew install --cask discord
 brew install --cask mos
 brew install --cask alfred
 brew install --cask aldente
 brew install --cask visual-studio-code
 brew install --cask postman
-
+brew install --cask github
+brew install --cask raycast
 
 echo "App installation completed."
 
