ca007a1e7ea769532d43dfb5758bf096efebf715
Tai Le <66931054+taiminhle4920@users.noreply.github.com>
2023-11-26 22:01:05 -0800

    Update app-installer.sh

--- mac-setup/app-installer.sh
+++ mac-setup/app-installer.sh
@@ -8,7 +8,6 @@ brew install --cask iterm2
 brew install --cask google-chrome
 brew install --cask discord
 brew install --cask mos
-brew install --cask alfred
 brew install --cask aldente
 brew install --cask visual-studio-code
 brew install --cask postman
